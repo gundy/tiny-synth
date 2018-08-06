3b26bf404c07ec167028b2f52a9c08818be978ab
gundy <gundy@users.noreply.github.com>
2018-08-06 04:55:53 -0700

    first attempt at adding a simple EWMA lowpass filter

---
+++ test/filter_ewma/filter_ewma.v
@@ -0,0 +1,7 @@
+// simple instantiation of the ewma filter module
+`ifndef __TINY_SYNTH_ROOT_FOLDER
+`define __TINY_SYNTH_ROOT_FOLDER ("../..")
+`endif
+
+`include "../../hdl/filter_ewma.vh"
+`include "../../hdl/clock_divider.vh"
